c29cc97a98bfbefe2e767aafc2606b56d82d9d47
Mar <marbou96@hotmail.com>
2026-03-31 11:30:56 +0200

    adding Vite api url in .env

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_URL || '/api/v1';
+
 const api = axios.create({
-    baseURL: 'api/v1',
+    baseURL: API_URL,
     headers: {
         'Content-Type': 'application/json',
     },
